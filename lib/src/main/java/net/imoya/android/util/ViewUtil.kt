11742003f545c6478b183b7026fc179fdc5e3c5e
IceImo-P <imoya.ice@gmail.com>
2023-11-27 00:31:10 +0900

    Release 1.15.2 - Add ViewUtil.overrideActivityTransition - Fix PowerUtil.isBackgroundRestricted returns incorrect value at prior to Android 9

--- lib/src/main/java/net/imoya/android/util/ViewUtil.kt
+++ lib/src/main/java/net/imoya/android/util/ViewUtil.kt
@@ -16,8 +16,12 @@
 
 package net.imoya.android.util
 
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 
 /**
  * Utilities for Android [View]s
@@ -77,5 +81,92 @@ object ViewUtil {
                 }
             }
         }
+    }
+
+    /**
+     * Call [Activity.overrideActivityTransition] or [Activity.overridePendingTransition]
+     *
+     * @param activity [Activity]
+     * @param isForClose true for OVERRIDE_TRANSITION_CLOSE, otherwise OVERRIDE_TRANSITION_OPEN. See [Activity.overrideActivityTransition]
+     * @param enterAnim See [Activity.overrideActivityTransition] or [Activity.overridePendingTransition]
+     * @param exitAnim See [Activity.overrideActivityTransition] or [Activity.overridePendingTransition]
+     */
+    @JvmStatic
+    fun overrideActivityTransition(
+        activity: Activity,
+        isForClose: Boolean,
+        enterAnim: Int,
+        exitAnim: Int
+    ) {
+        overrideActivityTransition(activity, isForClose, enterAnim, exitAnim, Color.TRANSPARENT)
+    }
+
+    /**
+     * Call [Activity.overrideActivityTransition] or [Activity.overridePendingTransition]
+     *
+     * @param activity [Activity]
+     * @param isForClose true for OVERRIDE_TRANSITION_CLOSE, otherwise OVERRIDE_TRANSITION_OPEN. See [Activity.overrideActivityTransition]
+     * @param enterAnim See [Activity.overrideActivityTransition] or [Activity.overridePendingTransition]
+     * @param exitAnim See [Activity.overrideActivityTransition] or [Activity.overridePendingTransition]
+     * @param backgroundColor See [Activity.overrideActivityTransition]
+     */
+    @JvmStatic
+    fun overrideActivityTransition(
+        activity: Activity,
+        isForClose: Boolean,
+        enterAnim: Int,
+        exitAnim: Int,
+        backgroundColor: Int
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            overrideActivityTransitionU(activity, isForClose, enterAnim, exitAnim, backgroundColor)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            overrideActivityTransitionT(activity, enterAnim, exitAnim, backgroundColor)
+        } else {
+            overrideActivityTransitionLegacy(activity, enterAnim, exitAnim)
+        }
+    }
+
+    /**
+     * [Activity.overrideActivityTransition] for UPSIDE_DOWN_CAKE or newer Android versions
+     */
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    private fun overrideActivityTransitionU(
+        activity: Activity,
+        isForClose: Boolean,
+        enterAnim: Int,
+        exitAnim: Int,
+        backgroundColor: Int
+    ) {
+        val overrideType: Int =
+            if (isForClose) Activity.OVERRIDE_TRANSITION_CLOSE
+            else Activity.OVERRIDE_TRANSITION_OPEN
+        activity.overrideActivityTransition(overrideType, enterAnim, exitAnim, backgroundColor)
+    }
+
+    /**
+     * [Activity.overridePendingTransition] for TIRAMISU
+     */
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    @Suppress("deprecation")
+    private fun overrideActivityTransitionT(
+        activity: Activity,
+        enterAnim: Int,
+        exitAnim: Int,
+        backgroundColor: Int
+    ) {
+        activity.overridePendingTransition(enterAnim, exitAnim, backgroundColor)
+    }
+
+    /**
+     * [Activity.overridePendingTransition] for older Android versions
+     */
+    @Suppress("deprecation")
+    private fun overrideActivityTransitionLegacy(
+        activity: Activity,
+        enterAnim: Int,
+        exitAnim: Int
+    ) {
+        activity.overridePendingTransition(enterAnim, exitAnim)
     }
 }
